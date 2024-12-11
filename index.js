0a51340ac5e1b42f149928f53a78f9f141947a85
Miguel Ángel <MAB015@HOTMAIL.COM>
2024-12-11 18:57:35 +0100

    Earth created

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
-import  { OrbitControls } from "..libs/jsm143/controls/OrbitControls.js";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import getStarfield from "./src/getStarfield.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 
 const fov = 75; // Camera frustum vertical field of view
 const aspect = w / h; //Camera frustum aspect ratio
@@ -17,4 +18,29 @@ const renderer = new THREE.WebGLRenderer({  antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-new OrbitalControls(camera, renderer.domElement);
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+
+new OrbitControls(camera, renderer.domElement);
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, 12);
+const material = new THREE.MeshStandardMaterial({
+    map: loader.load("./textures/00_earthmap1k.jpg")
+});
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
+
+const stars = getStarfield();
+// scene.add(stars);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
+
+function animate() {
+    requestAnimationFrame(animate);
+    earthMesh.rotation.y += 0.002;
+    renderer.render(scene, camera);
+}
+
+animate();
